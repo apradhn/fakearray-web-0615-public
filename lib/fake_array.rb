5fdc2cc2225bfec21c462244ed26b3bf22ce86d1
apradhn <abhijit.pradhan6@gmail.com>
2015-06-15 10:00:05 -0400

    all done

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,26 @@
+require "pry"
+
 class FakeArray
+  attr_accessor :array
 
   # you'll need a splat in this class somewhere
+  def initialize(*num)
+    @array = num
+  end
 
+  def [](num)
+    @array[num]
+  end
 
+  def each
+    size = @array.size
+    size.times do |i|
+      yield(@array[i-1])
+    end
+  end
+
+  def first
+    @array.first
+  end
 
 end
